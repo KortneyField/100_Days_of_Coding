4e08eec9873055e8897eed96504e40ace5abb878
KortneyField <kortneyfield@gmail.com>
2020-08-17 14:52:50 -0500

    added a search feature to a grocery list

--- js/apps.js
+++ js/apps.js
@@ -1,27 +1,15 @@
-const playlist = [
-    'So What',
-    'Respect',
-    'What a Wonderful World',
-    'At Last',
-    'Three Little Birds',
-    'The Way You Look Tonight'
-  ];
-  
-  function createListItems (arrayToPass) {
-    //creates an empty changable variable 
-    let items = ''; 
-    //iterates through the array that is passed
-    for (let i = 0; i < arrayToPass.length; i++) {
-      //creates li nodes with the pointed item in the array until loop is done.. 
-      items += `<li> ${ arrayToPass[i] } </li>`;
-    }
-    //give backs the content that was created in the for loop. 
-    return items; 
-  }
-  
-  // connects to the html section main and will insert the content after the = 
-  document.querySelector('main').innerHTML = 
-    //Makes an order list
-      //calls the function CreateListItems
-        //Passes the Playlist array 
-    `<ol> ${createListItems(playlist)} </ol> `; 
+const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
+
+const search = prompt('Search for a product.');
+
+let message;
+
+if (!search) {
+  message = `Nothing was entered`; 
+} else if (inStock.includes(search)){
+  message = `<strong>${search.toUpperCase()}</strong> IN STOCK. It's #${inStock.indexOf(search) +1} on the list.`; 
+} else { 
+  message = `${search} NOT IN STOCK`; 
+}
+
+document.querySelector('main').innerHTML = `<p>${message}</p>`
