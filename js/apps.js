b4da2efaaefbc79d9f13ac1ac04405848358d771
KortneyField <kortneyfield@gmail.com>
2020-08-17 14:16:12 -0500

    added a looping array to create an automated list on the html page

--- js/apps.js
+++ js/apps.js
@@ -1,7 +1,27 @@
-const middle = ['lettuce', 'cheese', 'patty'];
-const burger = ['top bun', ...middle, 'bottom bun'];
-
-
-for (i = 0; i < burger.length; ++i) {
-    console.log(burger[i]);
-}
+const playlist = [
+    'So What',
+    'Respect',
+    'What a Wonderful World',
+    'At Last',
+    'Three Little Birds',
+    'The Way You Look Tonight'
+  ];
+  
+  function createListItems (arrayToPass) {
+    //creates an empty changable variable 
+    let items = ''; 
+    //iterates through the array that is passed
+    for (let i = 0; i < arrayToPass.length; i++) {
+      //creates li nodes with the pointed item in the array until loop is done.. 
+      items += `<li> ${ arrayToPass[i] } </li>`;
+    }
+    //give backs the content that was created in the for loop. 
+    return items; 
+  }
+  
+  // connects to the html section main and will insert the content after the = 
+  document.querySelector('main').innerHTML = 
+    //Makes an order list
+      //calls the function CreateListItems
+        //Passes the Playlist array 
+    `<ol> ${createListItems(playlist)} </ol> `; 
