ba389b9b87730cec47ddae0d974db86715e59d6e
KortneyField <kortneyfield@gmail.com>
2020-08-17 13:49:01 -0500

    added practice wit arrays

--- js/apps.js
+++ js/apps.js
@@ -1,29 +1,7 @@
+const middle = ['lettuce', 'cheese', 'patty'];
+const burger = ['top bun', ...middle, 'bottom bun'];
 
-const main = document.querySelector('main');
-const randomNumber = getRandomNumber(5);
-let guess;
-let counter = 1;
-let guesses = 3;  
 
-function getRandomNumber(upper) {
-  return Math.floor( Math.random() * upper ) + 1;
+for (i = 0; i < burger.length; ++i) {
+    console.log(burger[i]);
 }
-console.log(randomNumber);
-
-do { 
-    guess = prompt(`You have ${guesses} left. Guess the Computer's number: ___ `)
-    counter++; 
-    guesses --; 
-    if (guess == randomNumber) {
-        main.innerHTML = `<h1> It took you ${counter} times to guessed the number ${randomNumber}! </h1>`;
-        break;
-    } else if (guess == 3) { 
-        main.innerHTML = `<h1> Too many guesses! </h1>`;
-        break; 
-    }
-} while (parseInt(guess) !== randomNumber); 
-    
-    
-    
-
-
