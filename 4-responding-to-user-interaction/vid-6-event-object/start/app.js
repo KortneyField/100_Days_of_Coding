554bc80c989620618f61b9f2b5231c844fe171d4
KortneyField <kortneyfield@gmail.com>
2020-08-24 12:45:32 -0500

    added a moseOver event handler to turn the list item to upperCase

--- 4-responding-to-user-interaction/vid-6-event-object/start/app.js
+++ 4-responding-to-user-interaction/vid-6-event-object/start/app.js
@@ -6,6 +6,16 @@ const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
+//const listItems = document.getElementsByTagName('li'); 
+
+/*for (let i=0; i< listItems.length; i = i+1) {
+  listItems[i].addEventListener('mouseover', () => {
+    listItems[i].textContent = listItems[i].textContent.toUpperCase();
+  });
+  listItems[i].addEventListener('mouseout', () => {
+    listItems[i].textContent = listItems[i].textContent.toLowerCase();
+  });
+}*/
 
 listDiv.addEventListener('mouseover', () => {
   listItems[i].textContent = listItems[i].textContent.toUpperCase();
@@ -13,6 +23,7 @@ listDiv.addEventListener('mouseover', () => {
 listDiv.addEventListener('mouseout', () => {
   listItems[i].textContent = listItems[i].textContent.toLowerCase();
 });
+
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
